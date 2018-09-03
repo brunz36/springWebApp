c81ef778626af7c6257f7ad20d88b01eaa88e8ea
Bruno Ribeiro <bribeiro@healthesystems.com>
2018-09-02 23:31:59 -0400

    Author Templates

---
+++ src/main/java/com/spring/webapp/controller/AuthorController.java
@@ -0,0 +1,23 @@
+package com.spring.webapp.controller;
+
+import com.spring.webapp.repository.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+	
+	private AuthorRepository authorRepository;
+	
+	public AuthorController(AuthorRepository authorRepository) {
+		this.authorRepository = authorRepository;
+	}
+	
+	@RequestMapping("/authors")
+	public String getAuthors(Model model) {
+		model.addAttribute("authors", authorRepository.findAll());
+		
+		return "authors";
+	}
+}
